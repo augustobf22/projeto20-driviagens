645ec86e4cc5df3389f96afb888f916428a477fe
augustof <augustobfanck@gmail.com>
2023-09-11 15:06:33 -0300

    chore: adjustement for deploy

--- src/app.js
+++ src/app.js
@@ -8,10 +8,11 @@ import errorHandler from "./middlewares/errorHandler.js";
 dotenv.config();
 const app = express();
 
+
 app.use(express.json());
 app.use(cors());
 app.use(router);
 app.use(errorHandler);
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
